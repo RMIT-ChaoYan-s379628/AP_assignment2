35f7f1ff86a94ff64ae3c2f1f81c5d781f5f624c
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 06:55:36 +0800

    Update ClassmateShip.java

--- assignment2/Main/ClassmateShip.java
+++ assignment2/Main/ClassmateShip.java
@@ -7,10 +7,9 @@
 */
 public class ClassmateShip extends RelationShip {
 
-	/* Inherit from RelationShip,include two Name*/
+	/* Inherit from RelationShip,include person1 and person2*/
 	public ClassmateShip(Person person1, Person person2) {
 		super(person1, person2);
-		// TODO Auto-generated constructor stub
 	}
 
 }
