c01cc9e3062c39709252d0f2208cfa0a8afdcc2d
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 04:15:17 +0800

    Update ColleagueShip.java

--- assignment2/Main/ColleagueShip.java
+++ assignment2/Main/ColleagueShip.java
@@ -1,4 +1,10 @@
 
+/*
+* @Author:Meng Gao	
+* @Date: 17/5/2018
+* @Introduction: 
+*
+*/
 public class ColleagueShip extends RelationShip {
 
 	public ColleagueShip(Person person1, Person person2) {
