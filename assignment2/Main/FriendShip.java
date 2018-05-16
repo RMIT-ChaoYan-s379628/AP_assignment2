8b81b9ac1d820b7cb9f9def82f57045ea65a6ce2
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 04:17:03 +0800

    Update FriendShip.java

--- assignment2/Main/FriendShip.java
+++ assignment2/Main/FriendShip.java
@@ -1,3 +1,10 @@
+
+/*
+* @Author:Meng Gao	
+* @Date: 17/5/2018
+* @Introduction: 
+*
+*/
 public class FriendShip extends RelationShip {
 	public FriendShip(Person person1, Person person2) {
 		super(person1, person2);
