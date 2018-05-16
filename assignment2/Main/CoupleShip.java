5186777c3fcaa0abc243f52b4695520fcd64688c
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 06:59:04 +0800

    Update CoupleShip.java

--- assignment2/Main/CoupleShip.java
+++ assignment2/Main/CoupleShip.java
@@ -7,6 +7,7 @@
 */
 public class CoupleShip extends RelationShip {
 
+	/* Inherit from RelationShip,include person1 and person2*/
 	public CoupleShip(Person person1, Person person2) {
 		super(person1, person2);
 	}
