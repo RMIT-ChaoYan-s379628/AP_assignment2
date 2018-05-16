30d58b1e7099ed9170b6b98b8c547c09f104ad79
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 06:28:53 +0800

    Update NotToBeColleaguesException.java

--- assignment2/Exception/NotToBeColleaguesException.java
+++ assignment2/Exception/NotToBeColleaguesException.java
@@ -8,8 +8,8 @@ package Exception;
 */
 public class NotToBeColleaguesException extends Exception {
 
-	/**
-	 * 
+	/*
+	 * The set of variable unchangeable
 	 */
 	private static final long serialVersionUID = 1L;
 
