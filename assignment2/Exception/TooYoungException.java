2de19c43419bbbf5326c1889bbb7a7265f503e65
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 04:29:53 +0800

    Add files via upload

--- assignment2/Exception/TooYoungException.java
+++ assignment2/Exception/TooYoungException.java
@@ -1,5 +1,12 @@
-package Exception;
 
-public class TooYoungException {
+public class TooYoungException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TooYoungException() {
+		super("Trying to make friend with a young child");
+	}
 }
