3a9e8d6958fb90aa4407fadaf67e76b5dc113743
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 04:21:13 +0800

    Update SiblingShip.java

--- assignment2/Main/SiblingShip.java
+++ assignment2/Main/SiblingShip.java
@@ -1,4 +1,10 @@
 
+/*
+* @Author:Meng Gao	
+* @Date: 17/5/2018
+* @Introduction: 
+*
+*/
 public class SiblingShip extends RelationShip {
 	//notice that sibling ship is the persons who have same father or same mother
 	public SiblingShip(Person person1, Person person2) {
