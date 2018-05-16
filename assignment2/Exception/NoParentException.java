6ecfbb4c875b1948a04d3e67816a86714e75d059
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 06:18:42 +0800

    Update NoParentException.java

--- assignment2/Exception/NoParentException.java
+++ assignment2/Exception/NoParentException.java
@@ -8,8 +8,8 @@ package Exception;
 */
 public class NoParentException extends Exception {
 
-	/**
-	 * 
+	/*
+	 * The set of variable unchangeable
 	 */
 	private static final long serialVersionUID = 1L;
 
