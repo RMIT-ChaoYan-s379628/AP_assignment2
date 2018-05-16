2a5ddfe5cc0198a5ce4e5b0c79bae37808e4b487
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 04:02:47 +0800

    Update ClassmateShip.java

--- assignment2/Main/ClassmateShip.java
+++ assignment2/Main/ClassmateShip.java
@@ -1,4 +1,10 @@
 
+/*
+* @Author:Meng Gao	
+* @Date: 17/5/2018
+* @Introduction: 
+*
+*/
 public class ClassmateShip extends RelationShip {
 
 	public ClassmateShip(Person person1, Person person2) {
