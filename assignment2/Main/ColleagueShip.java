39d79237bd5f3b4560f01828440c77c88e1cda14
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 06:56:59 +0800

    Update ColleagueShip.java

--- assignment2/Main/ColleagueShip.java
+++ assignment2/Main/ColleagueShip.java
@@ -7,6 +7,8 @@
 */
 public class ColleagueShip extends RelationShip {
 
+	/* Inherit from RelationShip,include person1 and person2*/
+	
 	public ColleagueShip(Person person1, Person person2) {
 		super(person1, person2);
 	}
