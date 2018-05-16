71956ad595a174fd684ae1540691c4c7a23e5964
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 03:25:04 +0800

    Update NoAvailableException.java

--- assignment2/Exception/NoAvailableException.java
+++ assignment2/Exception/NoAvailableException.java
@@ -1,14 +1,14 @@
 
-
 /*
  * Author:Meng Gao	
  * Date: 17/5/2018
  * Introduction: No connection between two adults
+ *Exceptions for adult
  */
 public class NoAvailableException extends Exception {
 
-	/**
-	 * 
+	/*
+	*The set of variable unchangeable
 	 */
 	private static final long serialVersionUID = 1L;
 
