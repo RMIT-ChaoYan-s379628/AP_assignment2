ff0342b88818c224bf7842c1a283a2e8be2a76d4
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 21:59:22 +1000

    Update ClassmateShip.java

--- MiniNet/src/Main/ClassmateShip.java
+++ MiniNet/src/Main/ClassmateShip.java
@@ -12,7 +12,7 @@ public class ClassmateShip extends RelationShip {
     /* Inherit from RelationShip,include person1 and person2*/
     public ClassmateShip(Person person1, Person person2) {
         super(person1, person2);
-        // TODO Auto-generated constructor stub
+        /*TODO Auto-generated constructor stub*/
     }
 
 }
