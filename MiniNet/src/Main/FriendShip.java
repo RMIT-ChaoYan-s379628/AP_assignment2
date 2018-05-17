5415fe16a5aedb9b9e88afaf5f25cc80b348dcbd
rmit-s3579628-ChaoYan <37400486+RMIT-ChaoYan-s379628@users.noreply.github.com>
2018-05-17 18:13:40 +1000

    Update FriendShip.java

--- MiniNet/src/Main/FriendShip.java
+++ MiniNet/src/Main/FriendShip.java
@@ -7,15 +7,19 @@ package Main;
 *
 */
 public class FriendShip extends RelationShip {
+	
+	/* Inherit from RelationShip*/
 	public FriendShip(Person person1, Person person2) {
 		super(person1, person2);
 		// TODO Auto-generated constructor stub
 	}
 
+	/* The get method of FirstPerson name*/
 	public Person getFirstPerson() {
 		return m_person1;
 	}
 
+	/* The get method of SecondPerson name*/
 	public Person getSecondPerson() {
 		return m_person2;
 	}
