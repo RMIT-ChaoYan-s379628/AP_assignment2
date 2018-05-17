e54053c31eb136171ce75714cef76db71c1e1ce2
rmit-s3579628-ChaoYan <37400486+RMIT-ChaoYan-s379628@users.noreply.github.com>
2018-05-17 17:04:31 +1000

    Update NoParentException.java

--- MiniNet/src/Exception/NoParentException.java
+++ MiniNet/src/Exception/NoParentException.java
@@ -13,6 +13,9 @@ public class NoParentException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/*
+	 * Exceptions of a child with no parent or only one parent
+	 */
 	public NoParentException() {
 		super("A child or young child has no parent or has only one parent.");
 	}
