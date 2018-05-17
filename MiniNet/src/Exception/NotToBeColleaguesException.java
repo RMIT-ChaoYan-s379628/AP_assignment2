c4c067531a4d024556d7a3e91f80c7bc6c24afa8
rmit-s3579628-ChaoYan <37400486+RMIT-ChaoYan-s379628@users.noreply.github.com>
2018-05-17 17:07:05 +1000

    Update NotToBeColleaguesException.java

--- MiniNet/src/Exception/NotToBeColleaguesException.java
+++ MiniNet/src/Exception/NotToBeColleaguesException.java
@@ -13,6 +13,9 @@ public class NotToBeColleaguesException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/*
+	 * Exceptions of child's connection in a colleague relation.
+	 */
 	public NotToBeColleaguesException() {
 		super("Trying to connect a child in a colleague relation.");
 	}
