d7d113a8189f0f10c94049a367bb5fe93eaae594
rmit-s3579628-ChaoYan <37400486+RMIT-ChaoYan-s379628@users.noreply.github.com>
2018-05-17 17:29:37 +1000

    Update Adult.java

--- MiniNet/src/Main/Adult.java
+++ MiniNet/src/Main/Adult.java
@@ -5,11 +5,12 @@ import java.util.LinkedList;
 /**
  * @author Chao Yan
  * @date 1/5/17 12:00
- * @description
+ * @description 
  */
 
 public class Adult extends Person {
 
+	/* Inherit from Person,include Name and Age*/
 	public Adult(String strName, int nAge, String strImage, String strStatus, String strSex, String strState) {
 		super(strName, nAge, strImage, strStatus, strSex, strState);
 		// TODO Auto-generated constructor stub
