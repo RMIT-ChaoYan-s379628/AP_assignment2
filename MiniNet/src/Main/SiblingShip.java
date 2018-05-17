1f852a3239424523fc4ec53b8b387d858f07d622
rmit-s3579628-ChaoYan <37400486+RMIT-ChaoYan-s379628@users.noreply.github.com>
2018-05-17 18:32:08 +1000

    Update SiblingShip.java

--- MiniNet/src/Main/SiblingShip.java
+++ MiniNet/src/Main/SiblingShip.java
@@ -7,10 +7,11 @@ package Main;
 *
 */
 public class SiblingShip extends RelationShip {
-	// notice that sibling ship is the persons who have same father or same
-	// mother
+	
+	/*notice that sibling ship is the persons who have same father or same mother*/
+	
 	public SiblingShip(Person person1, Person person2) {
 		super(person1, person2);
-		// TODO Auto-generated constructor stub
+		/* TODO Auto-generated constructor stub*/
 	}
 }
