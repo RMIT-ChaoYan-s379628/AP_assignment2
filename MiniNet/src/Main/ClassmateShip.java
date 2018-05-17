ee662ce04aa4c9078bf246035d05121b401ecea4
rmit-s3579628-ChaoYan <37400486+RMIT-ChaoYan-s379628@users.noreply.github.com>
2018-05-17 17:55:32 +1000

    Update ClassmateShip.java

--- MiniNet/src/Main/ClassmateShip.java
+++ MiniNet/src/Main/ClassmateShip.java
@@ -1,5 +1,12 @@
 package Main;
 
+/**
+* @Author:Meng Gao	
+* @Date: 17/5/2018
+* @Introduction: 
+*
+*/
+
 public class ClassmateShip extends RelationShip {
 
 	public ClassmateShip(Person person1, Person person2) {
