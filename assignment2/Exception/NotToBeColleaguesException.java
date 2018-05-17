e8d4e5c5b1819f77f8ed2b6f727a3abce444e73a
S3598281-meng-gao <37400665+S3598281-meng-gao@users.noreply.github.com>
2018-05-17 15:34:31 +1000

    Update NotToBeColleaguesException.java

--- assignment2/Exception/NotToBeColleaguesException.java
+++ assignment2/Exception/NotToBeColleaguesException.java
@@ -3,7 +3,7 @@ package Exception;
 /**
 * @Author:Meng Gao	
 * @Date: 17/5/2018
-* @Introduction: 
+* @Introduction: Exception for child's colleague relation.
 *
 */
 public class NotToBeColleaguesException extends Exception {
