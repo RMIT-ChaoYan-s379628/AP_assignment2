d781aa53da955971245877007ee82acc6130933b
rmit-s3579628-ChaoYan <37400486+RMIT-ChaoYan-s379628@users.noreply.github.com>
2018-05-17 18:03:57 +1000

    Update ColleagueShip.java

--- MiniNet/src/Main/ColleagueShip.java
+++ MiniNet/src/Main/ColleagueShip.java
@@ -1,7 +1,14 @@
 package Main;
 
+/**
+* @Author:Meng Gao	
+* @Date: 17/5/2018
+* @Introduction: 
+*
+*/
 public class ColleagueShip extends RelationShip {
 
+	/* Inherit from RelationShip,include person1 and person2*/
 	public ColleagueShip(Person person1, Person person2) {
 		super(person1, person2);
 	}
