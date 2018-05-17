84d4d89744544d75e01f4a2d3e9397026e18cd93
rmit-s3579628-ChaoYan <37400486+RMIT-ChaoYan-s379628@users.noreply.github.com>
2018-05-17 18:27:33 +1000

    Update Person.java

--- MiniNet/src/Main/Person.java
+++ MiniNet/src/Main/Person.java
@@ -13,8 +13,11 @@ import java.util.LinkedList;
 public class Person {
     protected int m_nAge;
     protected String m_strName, m_strImage, m_strStatus, m_strSex, m_strState;
+   
+    /* This is the list of every person has his/her friends. */
     private LinkedList<Person> m_lstParent = null;
 
+    /* Construction method,including name and age to construct a person */
     public Person(String strName, int nAge, String strImage, String strStatus, String strSex, String strState) {
         m_strName = strName;
         m_nAge = nAge;
@@ -25,46 +28,57 @@ public class Person {
         m_lstParent = new LinkedList<>();
     }
 
+    /* the set/get method of the Image */
     public void setImage(String strImage) {
         m_strImage = strImage;
     }
 
+    /* the set/get method of the Status */
     public void setStatus(String strStatus) {
         m_strStatus = strStatus;
     }
 
+    /* the set/get method of the Sex */
     public void setSex(String strSex) {
         m_strSex = strSex;
     }
 
+    /* the set/get method of the State */
     public void setState(String strState) {
         m_strState = strState;
     }
 
+    /* the set/get method of the Age */
     public int getAge() {
         return m_nAge;
     }
 
+    /* the set/get method of the Name */
     public String getName() {
         return m_strName;
     }
 
+    /* the set/get method of the Image*/
     public String getImage() {
         return m_strImage;
     }
 
+    /* the set/get method of the Status */
     public String getStatus() {
         return m_strStatus;
     }
 
+    /* the set/get method of the Sex */
     public String getSex() {
         return m_strSex;
     }
 
+    /* the set/get method of the State */
     public String getState() {
         return m_strState;
     }
 
+    /* the set/get method of the Parents */
     public LinkedList<Person> getParents() {
         return m_lstParent;
     }
@@ -72,7 +86,8 @@ public class Person {
     public boolean addParent(Person person) {
         if (null == person || 2 == m_lstParent.size())
             return false;
-        // can't have two fathers or two mothers
+        
+        /* can't have two fathers or two mothers*/
         for (int nIndex = 0; nIndex < m_lstParent.size(); nIndex++) {
             if (m_lstParent.get(nIndex).getSex().equals(person.getSex()))
                 return false;
